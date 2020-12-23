4ce8a387b2297cf4934feccaefa3d8580a7082e2
Sergey Poznyakoff <gray@gnu.org>
2020-12-23 09:21:18 +0200

    Remove duplicate global declarations
    * src/gdbm_dump.c: Add missing I18N.
    * src/gdbm_load.c: Likewise.
    * src/parseopt.c (parseopt_program_doc)
    (parseopt_program_args): Remove duplicate declarations.

--- src/gdbm_load.c
+++ src/gdbm_load.c
@@ -29,8 +29,8 @@ int mode;
 uid_t owner_uid;
 gid_t owner_gid;
 
-char *parseopt_program_doc = "load a GDBM database from a file";
-char *parseopt_program_args = "FILE [DB_FILE]";
+char *parseopt_program_doc = N_("load a GDBM database from a file");
+char *parseopt_program_args = N_("FILE [DB_FILE]");
 struct gdbm_option optab[] = {
   { 'r', "replace", NULL, N_("replace records in the existing database") },
   { 'm', "mode", N_("MODE"), N_("set file mode") },
