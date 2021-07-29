d3ffd42ac2dae174b8872485c486f09c856720e2
Sergey Poznyakoff <gray@gnu.org>
2021-07-29 15:48:21 +0300

    Mark GDBM_MAGIC constants as unsigned

--- src/gdbmconst.h
+++ src/gdbmconst.h
@@ -26,19 +26,19 @@
    This should've been done back when off_t was added to the library, but
    alas...  We just have to assume that an OMAGIC file is readable. */
 
-#define GDBM_OMAGIC		0x13579ace	/* Original magic number. */
-#define GDBM_MAGIC32	        0x13579acd	/* New 32bit magic number. */
-#define GDBM_MAGIC64		0x13579acf	/* New 64bit magic number. */
+#define GDBM_OMAGIC		0x13579aceu	/* Original magic number. */
+#define GDBM_MAGIC32	        0x13579acdu	/* New 32bit magic number. */
+#define GDBM_MAGIC64		0x13579acfu	/* New 64bit magic number. */
 
-#define GDBM_OMAGIC_SWAP	0xce9a5713	/* OMAGIC swapped. */
-#define GDBM_MAGIC32_SWAP	0xcd9a5713	/* MAGIC32 swapped. */
-#define GDBM_MAGIC64_SWAP	0xcf9a5713	/* MAGIC64 swapped. */
+#define GDBM_OMAGIC_SWAP	0xce9a5713u	/* OMAGIC swapped. */
+#define GDBM_MAGIC32_SWAP	0xcd9a5713u	/* MAGIC32 swapped. */
+#define GDBM_MAGIC64_SWAP	0xcf9a5713u	/* MAGIC64 swapped. */
 
-#define GDBM_NUMSYNC_MAGIC32    0x13579ad0
-#define GDBM_NUMSYNC_MAGIC64    0x13579ad1
+#define GDBM_NUMSYNC_MAGIC32    0x13579ad0u
+#define GDBM_NUMSYNC_MAGIC64    0x13579ad1u
 
-#define GDBM_NUMSYNC_MAGIC32_SWAP    0xd09a5713
-#define GDBM_NUMSYNC_MAGIC64_SWAP    0xd19a5713 
+#define GDBM_NUMSYNC_MAGIC32_SWAP    0xd09a5713u
+#define GDBM_NUMSYNC_MAGIC64_SWAP    0xd19a5713u
 
 /* Size of a hash value, in bits */
 #define GDBM_HASH_BITS 31
